5847420ab3c3377c9cac9bacd5a614fa332ab5e5
mohit jangid <mohitjangid183@gmail.com>
2022-07-11 23:16:28 +0530

    add scroller

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   Route
 } from "react-router-dom";
 export default class App extends Component {
+  // apiKey="2a3f682e90384faa8002a0e91007c3d5"
   render() {
     return (
       <>
@@ -16,14 +17,14 @@ export default class App extends Component {
           <NavBar />
           <Routes>
 
-            <Route exact path="/" element={<News key="general" pageSize={6} country="in" category="general" />} />
-            <Route exact path="/business" element={<News key="business" pageSize={6} country="in" category="business" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={6} country="in" category="entertainment" />} />
-            <Route exact path="/general" element={<News key="general" pageSize={6} country="in" category="general" />} />
-            <Route exact path="/health" element={<News key="health" pageSize={6} country="in" category="health" />} />
-            <Route exact path="/science" element={<News key="science" pageSize={6} country="in" category="science" />} />
-            <Route exact path="/sports" element={<News key="sports" pageSize={6} country="in" category="sports" />} />
-            <Route exact path="/technology" element={<News key="technology" pageSize={6} country="in" category="technology" />} />
+            <Route exact path="/" element={<News apiKey={this.apiKey} key="general" pageSize={6} country="in" category="general" />} />
+            <Route exact path="/business" element={<News apiKey={this.apiKey} key="business" pageSize={6} country="in" category="business" />} />
+            <Route exact path="/entertainment" element={<News apiKey={this.apiKey} key="entertainment" pageSize={6} country="in" category="entertainment" />} />
+            <Route exact path="/general" element={<News apiKey={this.apiKey} key="general" pageSize={6} country="in" category="general" />} />
+            <Route exact path="/health" element={<News apiKey={this.apiKey} key="health" pageSize={6} country="in" category="health" />} />
+            <Route exact path="/science" element={<News apiKey={this.apiKey} key="science" pageSize={6} country="in" category="science" />} />
+            <Route exact path="/sports" element={<News apiKey={this.apiKey} key="sports" pageSize={6} country="in" category="sports" />} />
+            <Route exact path="/technology" element={<News apiKey={this.apiKey} key="technology" pageSize={6} country="in" category="technology" />} />
           </Routes>
         </BrowserRouter>
       </>
